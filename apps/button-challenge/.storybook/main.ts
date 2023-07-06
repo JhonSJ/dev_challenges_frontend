bdab9be711e9a34b0755b52b4d1b2f979e19a148
JhonSJ <Tjhon12394@gmail.com>
2023-07-05 20:04:41 -0500

    feat(button-challenge): add deps

---
+++ apps/button-challenge/.storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)", "../stories/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        postCss: true,
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  logLevel: "debug",
+};
+export default config;
